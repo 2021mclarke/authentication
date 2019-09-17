596ea2315c755e6c4a01e1c74e71e14cfee1352b
2021mclarke <2021mclarke@dwight.edulgit config --global user.email 2021mclarke@dwight.edu>
2019-09-17 13:15:32 +0000

    first

---
+++ greeting.py
@@ -0,0 +1,8 @@
+name1 = "Diana"
+name2 = "Marlowe"
+name3 = 16
+
+print("Hello, " + name1)
+print(name2)
+print(name2 + " has " +  str(len(name2))  +  " characters in their name")
+print(name2 + " is " + str(name3) + " years old")
